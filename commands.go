bebe9d7163d8f6ba042f0196e25e274e0ba04e37
Swapnilgupta8585 <swapnilgupta19812004@gmail.com>
2024-10-21 13:37:20 +0530

    add map and mapb commands

--- commands.go
+++ commands.go
@@ -3,19 +3,36 @@ package main
 type cliCommand struct {
 	name        string
 	description string
-	callback    func() error
+	callback    func(*Config) error
 }
 
-var commandMap = map[string]cliCommand{
+type Config struct {
+	next     string
+	previous *string
+}
 
-	"help": {
-		name:        "help",
-		description: "Displays a help message",
-		callback:    commandHelp,
-	},
-	"exit": {
-		name:        "exit",
-		description: "Exit the Pokedex",
-		callback:    commandExit,
-	},
+func commands() map[string]cliCommand {
+	return map[string]cliCommand{
+
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+		"map": {
+			name:        "map",
+			description: "Let's you know the next 20 locations to explore",
+			callback:    commandMap,
+		},
+		"mapb": {
+			name:        "mapb",
+			description: "Let's you know the previous 20 locations to explore",
+			callback:    commandMapb,
+		},
+	}
 }
