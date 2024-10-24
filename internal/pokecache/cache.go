4c76768d5e6c3bdbc3c1930a64b87faa256de97b
Swapnilgupta8585 <swapnilgupta19812004@gmail.com>
2024-10-24 09:22:13 +0530

    update logic for caching

--- internal/pokecache/cache.go
+++ internal/pokecache/cache.go
@@ -7,7 +7,7 @@ import (
 
 type Cache struct {
 	entry map[string]cacheEntry
-	mu    *sync.Mutex
+	mu    sync.Mutex
 }
 
 type cacheEntry struct {
@@ -15,7 +15,7 @@ type cacheEntry struct {
 	val       []byte
 }
 
-func (c Cache) Add(key string, value []byte) {
+func (c *Cache) Add(key string, value []byte) {
 	c.mu.Lock()
 	defer c.mu.Unlock()
 	cacheEntryStruct := cacheEntry{
@@ -25,12 +25,23 @@ func (c Cache) Add(key string, value []byte) {
 	c.entry[key] = cacheEntryStruct
 }
 
-func (c Cache) reapLoop() {
-	c.mu.Lock()
-	defer c.mu.Unlock()
+func (c *Cache) reapLoop(interval time.Duration) {
+	ticker := time.NewTicker(interval)
+	defer ticker.Stop()
+	for {
+		<-ticker.C
+		c.mu.Lock()
+		for key, value := range c.entry {
+
+			if time.Since(value.createdAt) > interval {
+				delete(c.entry, key)
+			}
+		}
+		c.mu.Unlock()
+	}
 }
 
-func (c Cache) Get(key string) ([]byte, bool) {
+func (c *Cache) Get(key string) ([]byte, bool) {
 	c.mu.Lock()
 	defer c.mu.Unlock()
 	value, ok := c.entry[key]
@@ -40,27 +51,10 @@ func (c Cache) Get(key string) ([]byte, bool) {
 	return value.val, true
 }
 
-func NewCache(key string, value []byte, interval time.Duration, add bool) ([]byte, bool) {
-	entryValue := cacheEntry{
-		createdAt: time.Now(),
-		val:       value,
+func NewCache(interval time.Duration) *Cache {
+	cache := &Cache{
+		entry: make(map[string]cacheEntry),
 	}
-	entryMap := map[string]cacheEntry{
-		key: entryValue,
-	}
-	c := Cache{
-		entry: entryMap,
-		mu:    &sync.Mutex{},
-	}
-	if add {
-		c.Add(key, value)
-		return value, true
-	} else if !add {
-		mapValue, ok := c.Get(key)
-		if !ok {
-			return nil, false
-		}
-		return mapValue, true
-	}
-	return nil, false
+	go cache.reapLoop(interval)
+	return cache
 }
