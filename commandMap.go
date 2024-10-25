c2877d36a152858d2b80c3040b13e88a0f6b2938
Swapnilgupta8585 <swapnilgupta19812004@gmail.com>
2024-10-25 06:43:33 +0530

    update formatting

--- commandMap.go
+++ commandMap.go
@@ -8,15 +8,19 @@ import (
 
 func commandMap(cfg *Config, area string) error {
 	fmt.Println()
+	fmt.Println("location-area:")
 	locations, err := pokeapi.PokeapiCall(cfg.next)
 	if err != nil {
-		return fmt.Errorf("can not get the locations: %w", err)
+		fmt.Println()
+		fmt.Println("can not get the locations")
+		fmt.Println()
+		return nil
 	}
 	cfg.next = *locations.Next
 	cfg.previous = locations.Previous
 
 	for _, location := range locations.Results {
-		fmt.Printf("- %s\n", location.Name)
+		fmt.Printf(" -%s\n", location.Name)
 	}
 	fmt.Println()
 	return nil
