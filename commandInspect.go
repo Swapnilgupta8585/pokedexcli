661daf0bfab20bc47258b6e0c6f52781b0e157d8
Swapnilgupta8585 <swapnilgupta19812004@gmail.com>
2024-10-25 05:56:11 +0530

    add callback function for inspect command

---
+++ commandInspect.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+)
+
+func commandInspect(cfg *Config, pokemonName string) error {
+	pokemon, ok := cfg.user[pokemonName]
+	if !ok {
+		fmt.Println("you have not caught that pokemon")
+		return nil
+	}
+	fmt.Println()
+	fmt.Printf("Name: %s\n", pokemonName)
+	fmt.Printf("Height: %v\n", pokemon.Height)
+	fmt.Printf("Weight: %v\n", pokemon.Weight)
+	fmt.Println("Stats: ")
+	for _, stats := range pokemon.Stats {
+		fmt.Printf("  -%s: %v\n", stats.Stat.Name, stats.BaseStat)
+	}
+	fmt.Println("Types: ")
+	for _, types := range pokemon.Types {
+		fmt.Printf("  -%s\n", types.Type.Name)
+	}
+	fmt.Println()
+	return nil
+}
