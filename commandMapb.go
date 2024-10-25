91e638e75fa9c640a94591a336bbcd8e4a22a5fa
Swapnilgupta8585 <swapnilgupta19812004@gmail.com>
2024-10-25 06:44:00 +0530

    update formatting

--- commandMapb.go
+++ commandMapb.go
@@ -10,16 +10,23 @@ func commandMapb(cfg *Config, area string) error {
 	if cfg.previous != nil {
 		locations, err := pokeapi.PokeapiCall(*cfg.previous)
 		if err != nil {
-			return fmt.Errorf("can not get the locations: %w", err)
+			fmt.Println()
+			fmt.Println("can not get the locations")
+			fmt.Println()
+			return nil
 		}
 		fmt.Println()
+		fmt.Println("location-area:")
 		cfg.next = *locations.Next
 		cfg.previous = locations.Previous
 		for _, location := range locations.Results {
-			fmt.Printf("- %s\n", location.Name)
+			fmt.Printf(" -%s\n", location.Name)
 		}
 		fmt.Println()
 		return nil
 	}
-	return fmt.Errorf("can not view previous page as it has no locations")
+	fmt.Println()
+	fmt.Println("can not view previous page as it has no locations")
+	fmt.Println()
+	return nil
 }
