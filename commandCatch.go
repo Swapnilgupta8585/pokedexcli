72f30bbae5b6d66bb19b9a6330a8b9eb3e1b6ae8
Swapnilgupta8585 <swapnilgupta19812004@gmail.com>
2024-10-25 05:54:50 +0530

    update formatting

--- commandCatch.go
+++ commandCatch.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"time"
 
 	"github.com/Swapnilgupta8585/pokedexcli/internal/pokeapi"
 )
@@ -21,9 +22,10 @@ func commandCatch(cfg *Config, pokemonName string) error {
 		fmt.Printf("%s is already caught!\n", pokemonName)
 		return nil
 	}
+	fmt.Printf("Throwing a Pokeball at %s...\n", pokemonName)
+	time.Sleep(1 * time.Second)
 	randomNumber := rand.Float64() * 20
 	moreRandomNum := randomNumber + float64((pokemon.BaseExperience)/80)
-	fmt.Printf("Throwing a Pokeball at %s...\n", pokemonName)
 	if moreRandomNum <= 10.0 {
 		fmt.Printf("%s was caught!\n", pokemonName)
 		cfg.user[pokemonName] = pokemon
